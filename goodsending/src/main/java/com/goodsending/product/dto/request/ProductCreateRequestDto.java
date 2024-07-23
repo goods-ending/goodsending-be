ad5363dc6eeaa5214aa2a22ea49928c5bc85b42a
kimchaerin <puclpu18@gmail.com>
2024-07-23 17:18:29 +0900

    🐛 [#1] 상품 등록 요청 Dto Null 체크
    - ProductCreateRequestDto 의 상품명, 상품 소개 필드에 null 체크 Bean Validation 추가

--- goodsending/src/main/java/com/goodsending/product/dto/request/ProductCreateRequestDto.java
+++ goodsending/src/main/java/com/goodsending/product/dto/request/ProductCreateRequestDto.java
@@ -2,18 +2,21 @@ package com.goodsending.product.dto.request;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class ProductCreateRequestDto {
 
+  @NotNull(message = "상품명은 필수 입력 항목입니다.")
   @Size(min = 1, max = 20, message = "상품명은 1글자 이상 20자 이하로 입력할 수 있습니다.")
   private String name;
 
   @Min(value = 0, message = "판매가를 0원 이상 입력해야합니다.")
   private int price;
 
+  @NotNull(message = "상품 소개는 필수 입력 항목입니다.")
   @Size(min = 1, max = 255, message = "상품 소개는 1글자 이상 255자 이하로 입력할 수 있습니다.")
   private String introduction;
 

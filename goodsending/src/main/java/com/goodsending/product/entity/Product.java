59819f04081b69e7e66eeed0ccb2692daccb52cf
je-pa <76720692+je-pa@users.noreply.github.com>
2024-07-29 10:00:20 +0900

    Revert "✨ [#4] 찜 기능 추가 (#16)"
    This reverts commit 3e689d73db03f1a44a47b92b97124e2ed64dd175.

--- goodsending/src/main/java/com/goodsending/product/entity/Product.java
+++ goodsending/src/main/java/com/goodsending/product/entity/Product.java
@@ -45,9 +45,6 @@ public class Product extends BaseEntity {
   @Column(name = "bidding_count", nullable = false)
   private int biddingCount;
 
-  @Column(name = "like_count", nullable = false)
-  private Long likeCount;
-
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "member_id")
   private Member member;
@@ -81,8 +78,4 @@ public class Product extends BaseEntity {
         .member(member)
         .build();
   }
-  public void setLikeCount(Long likeCount) {
-    this.likeCount = likeCount;
-  }
-
 }

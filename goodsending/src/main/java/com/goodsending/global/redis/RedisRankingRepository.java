a7d20d2533e6cb26cd6dca3ad098fdf8fd99a0f6
kimchaerin <puclpu18@gmail.com>
2024-08-09 11:04:03 +0900

    🔥 [#85] unused code 삭제

--- goodsending/src/main/java/com/goodsending/global/redis/RedisRankingRepository.java
+++ goodsending/src/main/java/com/goodsending/global/redis/RedisRankingRepository.java
@@ -1,6 +1,5 @@
 package com.goodsending.global.redis;
 
-import java.time.Duration;
 import java.util.Set;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
@@ -18,10 +17,6 @@ public abstract class RedisRankingRepository<K, V> {
   public void setZSetValue(K key, V value, double score) {
     redisTemplate.opsForZSet()
         .add(PREFIX + key, value, score);
-  }
-
-  public void setExpire(K key, int expireMinutes) {
-    redisTemplate.expire(PREFIX + key, Duration.ofMinutes(expireMinutes));
   }
 
   public Set<TypedTuple<V>> getReverseZSetTupleByKey(K key, long start, long end) {

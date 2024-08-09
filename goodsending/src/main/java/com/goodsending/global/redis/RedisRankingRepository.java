6f200f05352cdaf1e2880e14069e6f2f041a0d71
kimchaerin <puclpu18@gmail.com>
2024-08-09 16:07:16 +0900

    🐛 [#85] Ranking Dto Repository 공통 사항 수정

--- goodsending/src/main/java/com/goodsending/global/redis/RedisRankingRepository.java
+++ goodsending/src/main/java/com/goodsending/global/redis/RedisRankingRepository.java
@@ -1,22 +1,32 @@
 package com.goodsending.global.redis;
 
 import java.util.Set;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 
+@RequiredArgsConstructor
 public abstract class RedisRankingRepository<K, V> {
 
   private final String PREFIX;
   private final RedisTemplate<String, V> redisTemplate;
 
-  public RedisRankingRepository(String PREFIX, RedisTemplate<String, V> redisTemplate) {
-    this.PREFIX = PREFIX;
-    this.redisTemplate = redisTemplate;
-  }
-
   public void setZSetValue(K key, V value, double score) {
     redisTemplate.opsForZSet()
         .add(PREFIX + key, value, score);
+  }
+
+  public void setHashValue(K key, String hashKey, V value) {
+    redisTemplate.opsForHash().put(PREFIX + key, hashKey, value);
+  }
+
+  public String getHashValueByKey(K key, String hashKey) {
+    return (String) redisTemplate.opsForHash()
+        .get(PREFIX + key, hashKey);
+  }
+
+  public Set<TypedTuple<V>> getZSetTupleByKey(K key, long start, long end) {
+    return redisTemplate.opsForZSet().rangeWithScores(PREFIX + key, start, end);
   }
 
   public Set<TypedTuple<V>> getReverseZSetTupleByKey(K key, long start, long end) {
@@ -36,4 +46,10 @@ public abstract class RedisRankingRepository<K, V> {
     redisTemplate.delete(PREFIX + key);
   }
 
+  public void deleteZSetValue(K key) {
+    Long size = redisTemplate.opsForZSet().size(PREFIX + key);
+    if (size != null && size > 0) {
+      redisTemplate.opsForZSet().removeRange(PREFIX + key, 0, size - 1);
+    }
+  }
 }

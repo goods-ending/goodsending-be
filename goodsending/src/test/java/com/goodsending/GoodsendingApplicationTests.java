1a5b7786fbd7c2e21ecf72be32ab8ada2f9d5ec5
je-pa <jieun0502@gmail.com>
2024-08-18 11:20:21 +0900

    ✅ [#190] GoodsendingApplication test

--- goodsending/src/test/java/com/goodsending/GoodsendingApplicationTests.java
+++ goodsending/src/test/java/com/goodsending/GoodsendingApplicationTests.java
@@ -1,10 +1,8 @@
 package com.goodsending;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class GoodsendingApplicationTests {
+class GoodsendingApplicationTests extends IntegrationTestSupport{
 
   @Test
   void contextLoads() {
